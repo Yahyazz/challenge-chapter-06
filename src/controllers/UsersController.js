ca5a438aaf73bae2574682a522aef00acfd84d66
Yahyazz <yahyaz.zakaria.4@gmail.com>
2024-05-29 15:36:31 +0700

    feat: CRUD user

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -5,11 +5,113 @@ import jwt from 'jsonwebtoken';
 export const getUsers = async (req, res) => {
   try {
     const users = await Users.findAll({
-      attributes: ['id', 'name', 'email'],
+      attributes: ['uuid', 'name', 'email', 'role', 'refresh_token'],
     });
     res.status(200).json(users);
   } catch (err) {
     res.status(500).json({ error: err.message });
+  }
+};
+
+export const getUserById = async (req, res) => {
+  try {
+    const user = await Users.findAll({
+      attributes: ['uuid', 'name', 'email', 'role', 'refresh_token'],
+      where: {
+        uuid: req.params.id,
+      },
+    });
+    res.status(200).json(user[0]);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const createUser = async (req, res) => {
+  const { name, email, password, confPassword, role } = req.body;
+  const emailExist = await Users.findAll({
+    where: {
+      email: email,
+    },
+  });
+  if (emailExist[0]) return res.status(400).json({ error: 'Email already exist' });
+
+  if (password !== confPassword)
+    return res.status(400).json({ error: 'Password and Confirm Password must be the same' });
+  const salt = await bcrypt.genSalt();
+  const hashPassword = await bcrypt.hash(password, salt);
+  try {
+    await Users.create({
+      name: name,
+      email: email,
+      password: hashPassword,
+      role: role,
+    });
+    res.status(201).json({
+      msg: 'User Created Successfully',
+    });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const user = await Users.findAll({
+    where: {
+      uuid: req.params.id,
+    },
+  });
+  if (!user[0]) return res.status(404).json({ error: 'User not found' });
+  const { name, email, password, confPassword, role } = req.body;
+  let hashPassword;
+  if (password === '' || password === null) {
+    hashPassword = user[0].password;
+  } else {
+    const salt = await bcrypt.genSalt();
+    hashPassword = await bcrypt.hash(password, salt);
+  }
+  if (password !== confPassword)
+    return res.status(400).json({ error: 'Password and Confirm Password must be the same' });
+  try {
+    await Users.update(
+      {
+        name: name,
+        email: email,
+        password: hashPassword,
+        role: role,
+      },
+      {
+        where: {
+          id: user[0].id,
+        },
+      }
+    );
+    res.status(201).json({
+      msg: 'User Updated Successfully',
+    });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  const user = await Users.findAll({
+    where: {
+      uuid: req.params.id,
+    },
+  });
+  if (!user[0]) return res.status(404).json({ error: 'User not found' });
+  try {
+    await Users.destroy({
+      where: {
+        id: user[0].id,
+      },
+    });
+    res.status(201).json({
+      msg: 'User Deleted Successfully',
+    });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
   }
 };
 
@@ -53,6 +155,13 @@ export const Login = async (req, res) => {
 
 export const Register = async (req, res) => {
   const { name, email, password, confPassword } = req.body;
+  const emailExist = await Users.findAll({
+    where: {
+      email: email,
+    },
+  });
+  if (emailExist[0]) return res.status(400).json({ error: 'Email already exist' });
+
   if (password !== confPassword)
     return res.status(400).json({ error: 'Password and Confirm Password must be the same' });
   const salt = await bcrypt.genSalt();
@@ -62,12 +171,13 @@ export const Register = async (req, res) => {
       name: name,
       email: email,
       password: hashPassword,
+      role: 'member',
     });
     res.status(201).json({
       msg: 'Register Success',
     });
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    res.status(400).json({ error: err.message });
   }
 };
 
@@ -91,4 +201,16 @@ export const Logout = async (req, res) => {
   );
   res.clearCookie('refreshToken');
   return res.sendStatus(200);
+};
+
+export const getCurrentUser = async (req, res) => {
+  const refreshToken = req.cookies.refreshToken;
+  if (!refreshToken) return res.sendStatus(204);
+  const user = await Users.findAll({
+    where: {
+      refresh_token: refreshToken,
+    },
+  });
+  if (!user[0]) return res.sendStatus(204);
+  res.json(user[0]);
 };
--- src/routes/UsersRoute.js
+++ src/routes/UsersRoute.js
@@ -1,14 +1,29 @@
 import express from 'express';
-import { getUsers, Register, Login, Logout } from '../controllers/UsersController.js';
+import {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  Register,
+  Login,
+  Logout,
+  getCurrentUser,
+} from '../controllers/UsersController.js';
 import { verifyToken } from '../middleware/verifyToken.js';
 import { refreshToken } from '../controllers/refreshToken.js';
 
 const router = express.Router();
 
-router.get('/users', verifyToken, getUsers);
-router.post('/register', Register);
-router.post('/login', Login);
+router.get('/users', getUsers);
+router.get('/users/:id', getUserById);
+router.post('/users/create', createUser); // superadmin
+router.put('/users/update/:id', updateUser); // superadmin
+router.delete('/users/delete/:id', deleteUser); // superadmin
+router.post('/register', Register); // member only
+router.post('/login', Login); // member only
 router.get('/token', refreshToken);
 router.delete('/logout', Logout);
+router.get('/whoami', verifyToken, getCurrentUser);
 
 export default router;
